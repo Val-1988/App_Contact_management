976befd73f92eca519ca6742b9ad1ce5b7aeff69
Valentina <123dog@gmail.com>
2023-01-05 16:40:19 +0300

    Added new Contact, save to DB

--- contact_management/app_contact_management/models.py
+++ contact_management/app_contact_management/models.py
@@ -12,4 +12,4 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.first_name, self.second_name, self.patronymic
+        return self.first_name + " " + self.second_name
--- contact_management/app_contact_management/views.py
+++ contact_management/app_contact_management/views.py
@@ -6,11 +6,23 @@ from .models import Contact
 
 def index(request):
     return render(request, 'index.html')
-    #contacts = Contact.objects.all()
-    #search_input = request.GET.get('search-area')
-    #if search_input:
-        #contacts = Contact.objects.filter(first_name_icontains=search_input)
+    # contacts = Contact.objects.all()
+    # search_input = request.GET.get('search-area')
+    # if search_input:
+    # contacts = Contact.objects.filter(first_name_icontains=search_input)
 
 
-#def addContact(request):
-    
+def addContact(request):
+    if request.method == "POST":
+        new_contact = Contact(
+            first_name=request.POST["first_name"],
+            second_name=request.POST["second_name"],
+            patronymic=request.POST["patronymic"],
+            organization=request.POST["organization"],
+            position=request.POST["position"],
+            email=request.POST['email'],
+            phone_number=request.POST['phone_number']
+        )
+        new_contact.save()
+        return redirect("/")
+    return render(request, 'new.html')
