50a0a41ea5e00cb2ad1a8f404ea83065a6c13623
Valentina <123dog@gmail.com>
2023-01-06 13:07:37 +0300

    Edit of contact_part_1

--- contact_management/app_contact_management/urls.py
+++ contact_management/app_contact_management/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_contact/', views.addContact, name='add_contact'),
+    path('profile/<str:pk>', views.contactProfile, name="profile"),
+    path('edit_contact/<str:pk>', views.editContact, name='edit_contact')
 
 ]
--- contact_management/app_contact_management/views.py
+++ contact_management/app_contact_management/views.py
@@ -5,11 +5,11 @@ from .models import Contact
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-    # contacts = Contact.objects.all()
+    contacts = Contact.objects.all()
     # search_input = request.GET.get('search-area')
     # if search_input:
     # contacts = Contact.objects.filter(first_name_icontains=search_input)
+    return render(request, 'index.html', {"contacts": contacts})
 
 
 def addContact(request):
@@ -20,9 +20,19 @@ def addContact(request):
             patronymic=request.POST["patronymic"],
             organization=request.POST["organization"],
             position=request.POST["position"],
-            email=request.POST['email'],
-            phone_number=request.POST['phone_number']
+            email=request.POST["email"],
+            phone_number=request.POST["phone_number"]
         )
         new_contact.save()
         return redirect("/")
-    return render(request, 'new.html')
+    return render(request, "new.html")
+
+
+def contactProfile(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, "contact-profile.html", {"contact": contact})
+
+
+def editContact(request, pk):
+    contact = Contact.objects.get(id=pk)
+    return render(request, "edit.html", {"contact": contact})
